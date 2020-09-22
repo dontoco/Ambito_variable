b3075ba72eca7d1e868671cc56b7a928796bf972
Victor <binomioalcubo@gmail.com>
2020-09-21 22:00:30 -0300

    first commit

---
+++ src/ambito_variable/Los_tres_ambitos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ambito_variable;
+
+/**
+ *
+ * @author Victor
+ */
+public class Los_tres_ambitos {
+    static int variableEstatica;
+    
+    int variableGlobal;
+    
+    void miMetodo(int paramertro){
+        int variableLocal = 0;
+    }
+    
+}
---
+++ src/ambito_variable/V_Local.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ambito_variable;
+
+/**
+ *
+ * @author Victor
+ */
+public class V_Local {
+    
+    void MiMetodo(int parametro){
+        
+        int variableLocal = 52;
+        
+        System.out.println("el valor de variableLocal es " +variableLocal);
+        System.out.println("El Valor de parametro es " + parametro);
+        
+    }
+    public void Prueba(){
+        System.out.println("");
+    }
+    
+    
+    
+}
